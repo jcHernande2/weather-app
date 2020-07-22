3593044ae9ccc1986dfa311be6666af31aaa26fd
juan carlos <hhjuan27@hotmail.com>
2020-07-21 19:27:10 -0500

    first commit

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+const WeatherLocation=()=>(
+    <div>
+        <Location city={"San Luis Potosí, México"}></Location>
+        <WeatherData></WeatherData>
+    </div>
+);
+export default WeatherLocation;
